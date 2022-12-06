42aa5e3589e2eada90f13694a8a2a9ffd5d2f28b
melonanna <anna2510@mail.ru>
2022-12-06 15:31:33 +0300

    task 4 done

---
+++ Homework_004/Program.cs
@@ -0,0 +1,20 @@
+﻿/* VERSION WITH WHILE
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int n=1;
+
+while (n<=N)
+{ if (n%2==0) Console.Write($" {n} ");
+n++;
+}
+*/
+/*VERSION WITH FOR*/
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+for (int n=1; n<=N;n++)
+{if(n%2==0)
+  Console.Write($" {n} ");}
