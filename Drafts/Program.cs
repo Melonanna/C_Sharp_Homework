7bb6680a75ddc153ee36041244b657bfcaf0522e
melonanna <anna2510@mail.ru>
2022-12-06 15:23:46 +0300

    task 4 done

--- Drafts/Program.cs
+++ Drafts/Program.cs
@@ -18,12 +18,65 @@ int. TryParse(number3, out c);
 /*?*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 */
 
+/*int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int n;
+for (int n>1; n<=N; n++)
+
+{
+Console.Write($" {n} ");
+}
+
+*/
+
+/*int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int n=1;
+
+while (n<=N)
+{ if (n%2==0) Console.Write($" {n} ");
+
+n++;
+}
+*/
+
 int N;
 Console.Write($"Введите число: ");
 int.TryParse(Console.ReadLine()!, out N);
 
+for (int n=1; n<=N;n++)
+{if(n%2==0)
+  Console.Write($" {n} ");}
 
-for (N%2==0,)
+
+
+
+
+/*for (int n>1; n<=N; n++)
+
+{
+Console.Write($" {n} ");
+}
+*/
+
+
+//int N;
+//Console.Write($"Введите число: ");
+//int.TryParse(Console.ReadLine()!, out N);
+
+//int n= -N;
+
+
+//while (n<=N)
+// {
+ // Console.Write($" {n} ");
+ // n++;
+
+ //} 
 
 
  
