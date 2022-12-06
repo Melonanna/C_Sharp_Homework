19ffee06decc03eddfb65c6e8eca8195e7996200
melonanna <anna2510@mail.ru>
2022-12-06 14:55:45 +0300

    saving

--- Drafts/Program.cs
+++ Drafts/Program.cs
@@ -1,4 +1,4 @@
-﻿int a;
+﻿/*int a;
 int b;
 int c;
 
@@ -13,5 +13,17 @@ int. TryParse(number2, out b);
 Console.Write("Введите третье число: ");
 string number3 = Console.ReadLine()!;
 int. TryParse(number3, out c);
+*/
+
+/*?*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+
+int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+
+for (N%2==0,)
+
 
  
