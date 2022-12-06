69e9887c53b975a103d4ed7d43c0a6878a5dd51a
melonanna <anna2510@mail.ru>
2022-12-06 12:53:49 +0300

    finished task 1

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,8 +1,10 @@
-﻿int a=5;
-int b=7;
+﻿int a;
+int b;
 
-Console.WriteLine($"Первое число - {a} ");
-Console.WriteLine($"Второе число - {b} ");
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
 
 if (a>b) {
  Console.WriteLine($"Наибольшее число - {a} "); 
@@ -12,31 +14,3 @@ else
   Console.WriteLine($"Наибольшее число - {b} ");
 }
 
-
-int d=2;
-int e=10;
-
-Console.WriteLine($"Первое число - {d} ");
-Console.WriteLine($"Второе число - {e} ");
-
-if (d>e) {
-  Console.WriteLine($"Наибольшее число - {d} "); 
-}
-else 
-{
-  Console.WriteLine($"Наибольшее число - {e} ");
-}
-
-int f=-9;
-int g=-3;
-
-Console.WriteLine($"Первое число - {f} ");
-Console.WriteLine($"Второе число - {g} ");
-
-if (f>g) {
-  Console.WriteLine($"Наибольшее число - {f} "); 
-}
-else 
-{
-  Console.WriteLine($"Наибольшее число - {g} ");
-}
