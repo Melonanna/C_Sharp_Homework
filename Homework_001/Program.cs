0e04449a368d04252ea4a2fd38d4aee8aff483b1
melonanna <anna2510@mail.ru>
2022-12-06 08:30:08 +0300

    first task is done

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a=5;
+int b=7;
+int max=a;
+
+Console.WriteLine($"Первое число - {a} ");
+Console.WriteLine($"Второе число - {b} ");
+
+if (a>max) {
+ Console.WriteLine($"Наибольшее число - {a} "); 
+}
+else 
+{
+  Console.WriteLine($"Наибольшее число - {b} ");
+}
+
+
+int d=2;
+int e=10;
+int more=d;
+
+Console.WriteLine($"Первое число - {d} ");
+Console.WriteLine($"Второе число - {e} ");
+
+if (d>more) {
+  Console.WriteLine($"Наибольшее число - {d} "); 
+}
+else 
+{
+  Console.WriteLine($"Наибольшее число - {e} ");
+}
+
+int f=-9;
+int g=-3;
+int m=d;
+
+Console.WriteLine($"Первое число - {f} ");
+Console.WriteLine($"Второе число - {g} ");
+
+if (f>m) {
+  Console.WriteLine($"Наибольшее число - {f} "); 
+}
+else 
+{
+  Console.WriteLine($"Наибольшее число - {g} ");
+}
