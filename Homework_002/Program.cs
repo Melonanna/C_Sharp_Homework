0255fd00520a8134c3b26cd940c328304afde493
melonanna <anna2510@mail.ru>
2022-12-06 13:47:29 +0300

    added to Github

---
+++ Homework_002/Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+int b;
+int c;
+int max;
+
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+Console.Write($"Введите третье число: ");
+int.TryParse(Console.ReadLine()!, out c);
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("Максимальное число - это ");
+Console.WriteLine(max);
