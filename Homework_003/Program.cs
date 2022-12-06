fed6f279ab8e04ad661862de53f25821ed12d06f
melonanna <anna2510@mail.ru>
2022-12-06 14:46:44 +0300

    task 3 done

---
+++ Homework_003/Program.cs
@@ -0,0 +1,14 @@
+﻿int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+Console.Write($"Является ли число четным? ");
+
+if (N%2==0)
+{
+  Console.Write($"Да");
+}
+
+else
+{
+  Console.Write($"Нет");
+}
