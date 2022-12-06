5e7906fce410da1b5a284ffcbc14e03261178af0
melonanna <anna2510@mail.ru>
2022-12-06 13:46:30 +0300

    added a few lines from seminar

---
+++ Drafts/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+int b;
+int c;
+
+Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine()!;
+int. TryParse(number1, out a);
+
+Console.Write("Введите второе число: ");
+string number2 = Console.ReadLine()!;
+int. TryParse(number2, out b);
+
+Console.Write("Введите третье число: ");
+string number3 = Console.ReadLine()!;
+int. TryParse(number3, out c);
+
+ 
