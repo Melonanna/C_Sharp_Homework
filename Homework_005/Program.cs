ac49b30bf2cae69d5f3b36ee6b8bfee0c435293f
melonanna <anna2510@mail.ru>
2022-12-07 17:36:12 +0300

    task 5 done

--- Homework_005/Program.cs
+++ Homework_005/Program.cs
@@ -1,16 +1,6 @@
 ﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. */
 
-int N;
 Console.Write($"Введите число: ");
-int.TryParse(Console.ReadLine()!, out N);
+string number = Console.ReadLine()!;
+Console.Write($"Цифра = {number[1]}");
 
-
-if (N%2==0)
-{
-  Console.Write($"Да");
-}
-
-else
-{
-  Console.Write($"Нет");
-}
