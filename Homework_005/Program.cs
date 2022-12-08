0884984fad1b43ba60e7b131bdb2d7f5e54a76e2
melonanna <anna2510@mail.ru>
2022-12-08 11:27:04 +0300

    task 5 done

--- Homework_005/Program.cs
+++ Homework_005/Program.cs
@@ -1,6 +1,17 @@
 ﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. */
 
-Console.Write($"Введите число: ");
-string number = Console.ReadLine()!;
-Console.Write($"Вторая цифра = {number[1]}");
+//Console.Write($"Введите число: ");
+//string number = Console.ReadLine()!;
+//Console.Write($"Вторая цифра = {number[1]}");
 
+
+int a;
+
+Console.Write($"Введите трехзначное число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+if (a>99 && a<1000)
+{Console.Write($"Вторая цифра = {a/10%10}");}
+
+else
+{Console.Write($"Ваше число не трехзначное. Повторите ввод");}
