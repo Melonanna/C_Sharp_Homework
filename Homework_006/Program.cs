21418452521e6fb3e85d5160ddc2372d183a305b
melonanna <anna2510@mail.ru>
2022-12-08 11:55:57 +0300

    not finished

--- Homework_006/Program.cs
+++ Homework_006/Program.cs
@@ -1,14 +1,22 @@
 ﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
 
-/*Console.Write($"Введите число: ");
-string num = Console.ReadLine()!;
+Console.Write($"Введите число: ");
+string number = Console.ReadLine()!;
 
-if (num<99)
-{Console.Write($"Такой цифры нет");}
-else
-{Console.Write($"Цифра = {number[2]}");}
-*/
+int a=number.length;
 
+if (a<2),
+
+{Console.Write($"Такой цифры нет");
+}
+else 
+{
+  Console.WriteLine(number[2]);
+  }
+
+
+
+/*
 int a;
 
 Console.Write($"Введите число: ");
@@ -16,9 +24,15 @@ int.TryParse(Console.ReadLine()!, out a);
 
 if (a>99) 
 {
-Console.Write($"Третья цифра = {a%10}");
+Console.Write($"Третья цифра = {(a/10)}");
 }
 else
 {
 {Console.Write($"Такой цифры нет");}
 }
+
+*/
+
+
+
+
