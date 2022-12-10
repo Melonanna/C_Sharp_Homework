f84db32696a815996fa114ecba74f4e4090765af
melonanna <anna2510@mail.ru>
2022-12-10 13:17:18 +0300

    all files updated

---
+++ Homework_010/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N;
+
+Console.Write($" Введите число N: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+for (i=1; i<=N; i++ )
+{
+Console.Write($"{N*N*N}");
+}
