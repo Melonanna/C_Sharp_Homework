9dafde885669ea59d227d95ac2724c1da74c7632
melonanna <anna2510@mail.ru>
2022-12-12 10:01:06 +0300

    task 13 done

--- Homework_013/Program.cs
+++ Homework_013/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int [] array= new int [8];
+
+for (int i=0;i<array.Length;i++)
+
+{
+ array[i] = new Random().Next(10);
+ Console.Write($" {array[i]} ");
+}
