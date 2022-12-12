f7c0b4f02b6ebf24ee1f9f6fe7a63e5f97434e0a
melonanna <anna2510@mail.ru>
2022-12-12 10:58:12 +0300

    task 12 done

--- Homework_012/Program.cs
+++ Homework_012/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int a;
+
+Console.Write($" Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int b=a;
+int sum=0;
+
+while (b>0)
+{sum = sum+b%10;
+b=b/10;
+}
+
+Console.Write($" Сумма цифр: {sum}");
