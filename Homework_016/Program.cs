547d6811e1e189b356c2dcd68dd7caa7276f090b
melonanna <anna2510@mail.ru>
2022-12-13 13:28:15 +0300

    task 16 done

--- Homework_016/Program.cs
+++ Homework_016/Program.cs
@@ -2,22 +2,25 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-int []array = new int[5];
+double []array = new double[5];
 for (int i=0; i<array.Length; i++)
-array[i] = new Random().Next(0,10);
+array[i] = new Random().Next(0,100)/10.0;
 Print(array);
 
-int max = 0;
+double max = array[0];
+double min = array[0];
 
-for (int i=0; i<array.Length; i++)
-     if (array[i] > array[i+1])
-     array[i]==max;
+for (int i=0; i<array.Length; i++) 
+ {  if (array[i] < min)
+    min = array [i];
+    if (array[i] > max)
+    max = array[i];
+}
+Console.Write($"Разница между max и min элементом массива - {Math.Round(max-min,2)}");
 
-Console.Write($"Максимальное число - {max}");
-
-void Print(int[]arr)
+void Print(double[]arr)
 {
-  for (int i=0; i<arr.Length; i++)
+ for (int i=0; i<arr.Length; i++)
     Console.Write($" {arr[i]} ");
   Console.WriteLine();
 }
