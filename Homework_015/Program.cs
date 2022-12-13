df94aab7f58c94f84555d976db8c87ac9d90147a
melonanna <anna2510@mail.ru>
2022-12-13 11:44:35 +0300

    task 15 done

--- Homework_015/Program.cs
+++ Homework_015/Program.cs
@@ -2,11 +2,26 @@
 //Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19   [-4, -6, 89, 6] -> 0
 
+int []array=new int[4];
+for (int i=0; i<array.Length; i++)
+  array[i] = new Random().Next(-99,100);
 
+Print(array);
 
+int sum=0;
 
-
-
+for (int i=1; i<array.Length; i=i+2)
+{
+ sum=sum+array[i];
+}
+Console.Write($"Сумма = {sum}");
+//функция вывода
+void Print(int[]arr)
+{
+  for (int i=0; i<arr.Length; i++)
+    Console.Write($" {arr[i]} ");
+  Console.WriteLine();
+}
 
 
 
