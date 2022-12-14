4031bdb63b6633bcda65381ce02ac074e51d6f6c
melonanna <anna2510@mail.ru>
2022-12-14 20:15:22 +0300

    task 18 done

--- Homework_018/Program.cs
+++ Homework_018/Program.cs
@@ -2,10 +2,27 @@
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double b1,k1,b2,k2;
 
-int b1,k1,b2,k2;
 
 Console.Write($" Введите число b1: ");
+double.TryParse(Console.ReadLine()!, out b1);
+Console.Write($" Введите число k1: ");
+double.TryParse(Console.ReadLine()!, out k1);
+Console.Write($" Введите число b2: ");
+double.TryParse(Console.ReadLine()!, out b2);
+Console.Write($" Введите число k2: ");
+double.TryParse(Console.ReadLine()!, out k2);
+
+if (k1!=k2)
+{ double x= (b2-b1)/(k1-k2);
+  double y= (k1*x)+b1;
+  Console.Write($" ({x};{y}) ");
+}
+else
+  Console.Write($"Данные прямые параллельны (не пересекаются)");
+
+/*Console.Write($" Введите число b1: ");
 int.TryParse(Console.ReadLine()!, out b1);
 Console.Write($" Введите число k1: ");
 int.TryParse(Console.ReadLine()!, out k1);
@@ -16,3 +33,9 @@ int.TryParse(Console.ReadLine()!, out k2);
 
 int x,y;
 
+*/
+
+//double b1=2;
+//double k1=5;
+//double b2=4;
+//double k2=9;
