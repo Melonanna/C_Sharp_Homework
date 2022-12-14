365ea386df7d4e6ad3963de1c5e3375708a05c77
melonanna <anna2510@mail.ru>
2022-12-14 19:01:51 +0300

    in progress

--- Homework_017/Program.cs
+++ Homework_017/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-
+/*
 int []array=new int[5];
 for (int i=0; i<array.Length; i++)
 array[i]=new Random().Next(-1000,1000);
@@ -21,3 +21,29 @@ void Print(int[]arr)
     Console.Write($" {arr[i]} ");
   Console.WriteLine();
 }
+
+*/
+
+
+int m;
+Console.Write($" Введите числа: ");
+int.TryParse(Console.ReadLine()!, out m);
+
+int []array=new int[m];
+
+for (int i = 0; i<m; i++)
+  Console.Write($"[{i}] = ");
+  int.TryParse(Console.ReadLine()!, out array[i]);
+
+int k=0;
+foreach (var int i in array)
+  if (i>0)
+  k++;
+
+/*int count =0;
+for (int i=0; i<m; i++)
+   {if (i>0)
+count=count+1;
+   }
+  */
+Console.Write($"{k} чисел больше 0");
