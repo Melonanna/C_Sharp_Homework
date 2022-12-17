d672fb81c6845285acb28b250b564fc39fae565e
melonanna <anna2510@mail.ru>
2022-12-17 13:21:37 +0300

    task 19 done

--- Homework_019/Program.cs
+++ Homework_019/Program.cs
@@ -5,26 +5,18 @@
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
 
+double[,] array = new double [3,4];
 
-double m,n;
-Console.Write($"Введите m размерность массива:");
-double.TryParse(Console.ReadLine(), out m);
-Console.Write($"Введите n размерность массива:");
-double.TryParse(Console.ReadLine(), out n);
-
-double[,] array = new double[m,n];
-
-
-// void PrintArray(int[,] arr)
-{ for(int i=0; i<array.GetLength(0);i++)
+for(int i=0; i<array.GetLength(0);i++)
 {
 for (int j=0; j<array.GetLength(1);j++)
 {
-  array[i,j] = new Random().Next(10);
-  Console.Write($" {array[i,j]} ");
+  array[i,j] = new Random().Next(-99,100)/10.0;
+  Console.Write("{0, 5}",array[i,j] ); 
 }
  Console.WriteLine();
 }
-}
+
+
 
 
