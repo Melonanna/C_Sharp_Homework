94687f985130473aed7d74f678632dd30f0b49e3
melonanna <anna2510@mail.ru>
2022-12-19 08:08:20 +0300

    task 21 done

--- Homework_021/Program.cs
+++ Homework_021/Program.cs
@@ -1,9 +1,19 @@
 ﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
-/*Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-*/
+int [,]array = new int [3,4];
+for (int i=0; i<array.GetLength(0);i++)
+{
+  for (int j=0; j<array.GetLength(1);j++)
+  {
+  array [i,j]=new Random().Next(10);
+  Console.Write("{0, 3}",array[i,j] ); 
+  }
+  Console.WriteLine();
+}
+for (int j=0; j<array.GetLength(1); j++)
+{ int sum = 0;
+  for (int i = 0; i<array.GetLength(0); i++)
+  sum = sum + array[i,j];
+ Console.Write($"{Math.Round((double)sum / array.GetLength(0), 1)} ");
+}
