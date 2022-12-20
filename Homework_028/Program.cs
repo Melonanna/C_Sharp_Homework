d77d9ef36c775cd4b2c635d7e40cf5f6e7232482
melonanna <anna2510@mail.ru>
2022-12-20 21:01:14 +0300

    task 28 done

---
+++ Homework_028/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+int m,n;
+Console.Write($"Введите M:");
+int.TryParse(Console.ReadLine()!, out m);
+Console.Write($"Введите N:");
+int.TryParse(Console.ReadLine()!, out n);
+
+string PrintNums(int left,int right)
+{
+  if (left>=right)
+     return right.ToString();
+  return $"{left} {PrintNums(left+1,right)}";
+}
+int sum=0;
+int count;
+  if (m>n) count=m-n+1;
+  else count=n-m+1;
+
+sum=(m+n)*count/2;
+Console.Write($"Сумма элементов: {sum} ");
